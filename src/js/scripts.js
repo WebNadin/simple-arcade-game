efb40499caa576a7e431163ad5a0b8ad0526d66f
WebNadin <nenadinweb@gmail.com>
2020-05-25 17:16:14 +0300

    pixi first stage

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,31 +1,22 @@
 //= partials/helper.js
 //= partials/pixi.min.js
 
-const stage = new PIXI.Container();
-const app = new PIXI.Application();
-document.body.appendChild(app.view);
-
-app.loader.add('bunny', '/images/bunny.png').load((loader, resources) => {
-    // This creates a texture from a 'bunny.png' image
-    const bunny = new PIXI.Sprite(resources.bunny.texture);
-
-    // Setup the position of the bunny
-    bunny.x = app.renderer.width / 2;
-    bunny.y = app.renderer.height / 2;
-
-    // Rotate around the center
-    bunny.anchor.x = 0.5;
-    bunny.anchor.y = 0.5;
-
-    // Add the bunny to the scene we are building
-    app.stage.addChild(bunny);
-
-    // Listen for frame updates
-    app.ticker.add(() => {
-         // each frame we spin the bunny around a bit
-        bunny.rotation += 0.01;
-    });
+let sizeStep = 8;
+const app = new PIXI.Application({
+    width: sizeStep*61, height: sizeStep*39, backgroundColor: 0xffffff, resolution: window.devicePixelRatio || 1
 });
+document.getElementById('generationArea').appendChild(app.view);
+
+app.view.style.position = "absolute";
+app.view.style.width = document.getElementById('generationArea').innerWidth;
+app.view.style.height = document.getElementById('generationArea').innerHeight;
+app.view.style.display = "block";
+app.view.style.boxSizing = "border-box";
+
+
+const container = new PIXI.Container();
+
+app.stage.addChild(container);
 
 
 
