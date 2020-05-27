5a482e745f44b549041396eae219ef7c5f15fcbb
WebNadin <nenadinweb@gmail.com>
2020-05-27 04:53:30 +0300

    ellipse and triangle shapes were added

--- src/js/partials/shapes.js
+++ src/js/partials/shapes.js
@@ -3,26 +3,57 @@ class Shape {
   constructor(color = '0xFFFF0B', type = 'circle', generationAreaWidth = 488, app) {
     this.color = color;
     this.type = type;
-    this.radius = 50;
+    this.radius = 40;
     this.app = app;
-    this.inAreaX = generationAreaWidth - 100;
-    this.circleY = -50;
-    this.circleX = 50 + random(this.inAreaX);
+    this.circleY = -this.radius;
+    this.circleX = this.radius + random(generationAreaWidth - this.radius * 2);
+    this.ellipseX = this.radius * 2 + random(generationAreaWidth - this.radius * 4);
+    this.ellipseY = -this.radius;
+    this.triangleX = random(generationAreaWidth - this.radius * 2);
+    this.triangleY = this.radius*2;
+    this.borderColor = 0x000000;
   }
 
   createCircle() {
     this.circle = new PIXI.Graphics();
-    this.circle.lineStyle(0);
+    this.circle.lineStyle(2, this.borderColor, 1);
     this.circle.beginFill(this.color, 1);
     this.circle.drawCircle(this.circleX, this.circleY, this.radius);
     this.circle.endFill();
     this.circle.interactive = true;
     this.circle.buttonMode = true;
     this.circle.live = true;
-    //figuresAmount++;
-    //circle.num = figuresAmount;
     figures.push(this.circle);
     this.app.stage.addChild(this.circle);
+  }
+
+  createEllipse() {
+    this.ellipse = new PIXI.Graphics();
+    this.ellipse.lineStyle(2, this.borderColor, 1);
+    this.ellipse.beginFill(this.color, 1);
+    this.ellipse.drawEllipse(this.ellipseX, this.ellipseY, this.radius * 2, this.radius);
+    this.ellipse.endFill();
+    this.ellipse.interactive = true;
+    this.ellipse.buttonMode = true;
+    this.ellipse.live = true;
+
+    figures.push(this.ellipse);
+    this.app.stage.addChild(this.ellipse);
+  }
+
+  createTriangle() {
+    this.triangle = new PIXI.Graphics();
+    this.triangle.lineStyle(2, 0xffd900, 1);
+    this.triangle.beginFill(0xFF3300);
+    this.triangle.moveTo(this.triangleX, 0);
+    this.triangle.lineTo(this.triangleX + this.radius, -this.triangleY);
+    this.triangle.lineTo(this.triangleX + this.radius*2, 0);
+    this.triangle.lineTo(this.triangleX, 0);
+    this.triangle.closePath();
+    this.triangle.endFill();
+
+    figures.push(this.triangle);
+    this.app.stage.addChild(this.triangle);
   }
 
   startFallingAnimation() {
@@ -45,7 +76,12 @@ class Shape {
     switch (this.type) {
       case 'circle':
         this.createCircle();
-
+        break;
+      case 'ellipse':
+        this.createEllipse();
+        break;
+      case 'triangle':
+        this.createTriangle();
         break;
     }
     this.startFallingAnimation();
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -6,7 +6,9 @@
 let figures = [];
 let animationStep = 1;
 let gravity = 4;
-
+//let shapes = ['circle', 'ellipse', 'triangle', 'rectangle', 'pentagon', 'hexagon'];
+let shapes = ['circle', 'ellipse', 'triangle'];
+let colors = [0xFFFF0B, 0xFF700B, 0x4286f4, 0x4286f4, 0xf441e8, 0x8dff6d, 0x41ccc9, 0xe03375, 0x95e032, 0x77c687, 0x43ba5b, 0x0ea3ba];
 
 class generationArea {
 
@@ -17,9 +19,6 @@ class generationArea {
     this.gravity = 4;
     this.shapesAmount = -1;
     this.activeShapesAmount = 0;
-    //this.shapes = ['circle', 'ellipse', 'triangle', 'rectangle', 'pentagon', 'hexagon'];
-    this.shapes = ['circle', 'circle', 'circle'];
-    this.colors = [0xFFFF0B, 0xFF700B, 0x4286f4, 0x4286f4, 0xf441e8, 0x8dff6d, 0x41ccc9, 0xe03375, 0x95e032, 0x77c687, 0x43ba5b, 0x0ea3ba];
     this.app = new PIXI.Application({
       width: this.width, height: this.height, backgroundColor: 0xffffff, resolution: window.devicePixelRatio || 1
     });
@@ -37,8 +36,8 @@ class generationArea {
   }
 
   createRandomShape() {
-    this.randomColor = randomElem(this.colors);
-    this.randomShape = randomElem(this.shapes);
+    this.randomColor = randomElem(colors);
+    this.randomShape = randomElem(shapes);
     this.shape = new Shape(this.randomColor, this.randomShape, this.width, this.app);
     this.shape.render();
   }
